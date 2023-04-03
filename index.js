85fcd578ebbc3aa3a83d575e82409988c83bfd6a
Sahanon Phisetpakasit <pingotakulml@gmail.com>
2023-04-03 16:03:09 +0700

    disable path

--- index.js
+++ index.js
@@ -47,51 +47,51 @@ app.post("/arbitrage", async function(req, res){
     }).status(200);
 })
 
-app.get("/info", function (req, res){
-    Logger.info(`/info is called`);
-    axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=thb", {timeout: 3000}).then((response) => {
-        var data = response.data;
-        fs.writeFileSync("data.json", JSON.stringify(data));
-        Logger.info(`/info API called success`);
-        res.send(data);
+// app.get("/info", function (req, res){
+//     Logger.info(`/info is called`);
+//     axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=thb", {timeout: 3000}).then((response) => {
+//         var data = response.data;
+//         fs.writeFileSync("data.json", JSON.stringify(data));
+//         Logger.info(`/info API called success`);
+//         res.send(data);
         
-    }).catch((err)=> {
-        Logger.err(err);
-        var data = fs.readFileSync("data/data.json");
-        res.send(JSON.parse(data)).status(200);
-    })
+//     }).catch((err)=> {
+//         Logger.err(err);
+//         var data = fs.readFileSync("data/data.json");
+//         res.send(JSON.parse(data)).status(200);
+//     })
     
-})
+// })
 
-app.get("/graph/:id/:day", function(req, res){
-    var id = req.params.id
-    var day = req.params.day
-    Logger.info(`/graph/${id}/${day} is called`);
-    var range = "";
-    if (day == 1){
-        range = "today";
-    }
-    else if (day == 7){
-        range = "week";
-    }
-    else if (day == 30){
-        range = "month";
-    }
-    else if (day == 365){
-        range = "year"
-    }
-    axios.get(`https://api.coingecko.com/api/v3/coins/${id}/ohlc?vs_currency=thb&days=${day}`).then((response) => {
-        var data = response.data
-        fs.writeFileSync(`data/graph/${range}/${id}.json`,JSON.stringify(data));
-        Logger.info(`/graph/${id}/${day} API called success`);
-        res.send(data).status(200);
-    }).catch((err) => {
-        Logger.err(err);
-        var data = fs.readFileSync(`data/graph/${range}/${id}.json`);
-        res.send(JSON.parse(data)).status(200);
+// app.get("/graph/:id/:day", function(req, res){
+//     var id = req.params.id
+//     var day = req.params.day
+//     Logger.info(`/graph/${id}/${day} is called`);
+//     var range = "";
+//     if (day == 1){
+//         range = "today";
+//     }
+//     else if (day == 7){
+//         range = "week";
+//     }
+//     else if (day == 30){
+//         range = "month";
+//     }
+//     else if (day == 365){
+//         range = "year"
+//     }
+//     axios.get(`https://api.coingecko.com/api/v3/coins/${id}/ohlc?vs_currency=thb&days=${day}`).then((response) => {
+//         var data = response.data
+//         fs.writeFileSync(`data/graph/${range}/${id}.json`,JSON.stringify(data));
+//         Logger.info(`/graph/${id}/${day} API called success`);
+//         res.send(data).status(200);
+//     }).catch((err) => {
+//         Logger.err(err);
+//         var data = fs.readFileSync(`data/graph/${range}/${id}.json`);
+//         res.send(JSON.parse(data)).status(200);
 
-    })
-})
+//     })
+// })
 
 app.get("/health", function(req, res){
     res.send("ok").status(200);
